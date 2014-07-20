00dd8a59fdaee42a12fe3c966d1f400c14aba7b7
smrmkt <smrmkt@gmail.com>
2014-07-20 11:08:27 +0900

    modify problem lines

--- problem_004.py
+++ problem_004.py
@@ -2,9 +2,9 @@
 #-*-coding:utf-8-*-
 
 '''
-If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
 import timeit
