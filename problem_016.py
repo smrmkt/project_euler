d68afc1ddf2bcf6a25ed44b23a82e7e12cbbbf92
smrmkt <smrmkt@gmail.com>
2014-08-08 12:11:32 +0900

    add 16

---
+++ problem_016.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+'''
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2^1000?
+'''
+
+import timeit
+
+
+def loop(n):
+        chars = str(2**n)
+        return sum([int(chars[i]) for i in range(len(chars))])
+
+if __name__ == '__main__':
+    print loop(1000)
+    print timeit.Timer('problem_016.loop(1000)', 'import problem_016').timeit(10)
