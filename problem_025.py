bf06ff69a1d20358c219b0914339af62d5184ac3
smrmkt <smrmkt@gmail.com>
2014-08-24 13:51:28 +0900

    Revert "add problem 27"
    This reverts commit b220891abb1e77120086926adb607c4a9b6453df.

--- problem_025.py
+++ problem_025.py
@@ -15,7 +15,7 @@ F6 = 8
 F7 = 13
 F8 = 21
 F9 = 34
-F10 = 55x
+F10 = 55
 F11 = 89
 F12 = 144
 
@@ -23,23 +23,23 @@ The 12th term, F12, is the first term to contain three digits.
 What is the first term in the Fibonacci sequence to contain 1000 digits?
 '''
 
-from problem_003 import Loop
 import timeit
 
 
 def loop(order):
-    for a in range(-999, 1000):
-        for b in range(-999, 1000):
-            Loop().is_prime()
+    f1 = 1
+    f2 = 1
+    cnt = 2
+    while len(str(f2)) < order:
+        f1, f2 = fibonacchi(f1, f2)
+        cnt += 1
+    return cnt
 
+
+def fibonacchi(f1, f2):
+    return f2, f1+f2
 
 
 if __name__ == '__main__':
     print loop(1000)
     print timeit.Timer('problem_025.loop(1000)', 'import problem_025').timeit(10)
-
-    def is_prime(self, n):
-        for i in range(2, int(math.sqrt(n))+1):
-            if n % i == 0:
-                return False
-        return True
