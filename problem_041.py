03f718689834006cd516f4cec42d42639dea1446
smrmkt <smrmkt@gmail.com>
2014-09-15 14:39:03 +0900

    fix bugs

--- problem_041.py
+++ problem_041.py
@@ -7,9 +7,8 @@
 import math
 import timeit
 
-
-def loop(n):
-    for i in range(n, 1, -1):
+def loop():
+    for i in range(7654321, 1234567, -1):
         if is_pandigital(i):
             if is_prime(i):
                 return i
@@ -19,13 +18,13 @@ def is_prime(n):
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return False
-        return True
+    return True
 
 def is_pandigital(n):
-    if len(set(list(str(n)))) == len(str(n)):
+    if set(list(str(n))) == set(map(str, range(1, int(math.log10(n)+2)))):
         return True
     return False
 
 if __name__ == '__main__':
-    print loop(7654321)
-    print timeit.Timer('problem_041.loop(7654321)', 'import problem_041').timeit(1)
+    print loop()
+    print timeit.Timer('problem_041.loop()', 'import problem_041').timeit(1)
